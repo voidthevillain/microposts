f4ebfcb285469505afe1c04fd0aada911a8a8d01
Mihai Filip <voidthevillain@users.noreply.github.com>
2019-11-28 15:59:25 +0100

    implement adding posts and alerts

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ function submitPost(e) {
   http
     .post('http://localhost:3000/posts', data)
     .then(data => {
+      ui.showAlert('Post successfully added.', 'alert alert-success mt-3 text-center')
+      ui.clearFields()
       getPosts()
     })
     .catch(err => console.log(err))
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,5 @@
+import { timingSafeEqual } from "crypto"
+
 class UI {
   constructor() {
     this.post = document.querySelector('#posts')
@@ -17,10 +19,10 @@ class UI {
           <div class="card-body">
             <h4 class="card-title">${post.title}</h4>
             <p class="card-text">${post.body}</p>
-            <a href="#" class="edit card-link" data-id="${post.id}">
+            <a href="#" class="edit card-link text-secondary" data-id="${post.id}">
               <i class="fa fa-pencil"></i>
             </a>
-            <a href="#" class="delete card-link" data-id="${post.id}">
+            <a href="#" class="delete card-link text-danger" data-id="${post.id}">
               <i class="fa fa-remove"></i>
             </a>
           </div>
@@ -29,6 +31,36 @@ class UI {
     })
 
     this.post.innerHTML = output
+  }
+
+  showAlert(message, className) {
+    this.clearAlert()
+
+    const div = document.createElement('div')
+    div.className = className
+    div.innerHTML = `<strong>${message}</strong>`
+
+    const container = document.querySelector('.posts-container')
+    const posts = document.querySelector('#posts')
+
+    container.insertBefore(div, posts)
+
+    setTimeout(() => {
+      this.clearAlert()
+    }, 3000)
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert')
+
+    if (currentAlert) {
+      currentAlert.remove()
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = ''
+    this.bodyInput.value = ''
   }
 }
 

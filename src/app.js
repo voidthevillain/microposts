a12f1ba8ac2b3fc00cd7ff908b6e7ca1955b4305
Mihai Filip <voidthevillain@users.noreply.github.com>
2019-11-28 15:24:44 +0100

    add submit post to db

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import { ui } from './ui'
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
+// Listen for post submit
+document.querySelector('.post-submit').addEventListener('click', submitPost)
+
+// Get posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -11,4 +15,24 @@ function getPosts() {
       ui.showPosts(data)
     })
     .catch(err => console.log(err))
+}
+
+// Submit post
+function submitPost(e) {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+  const data = {
+    title: title,
+    body: body
+  }
+
+  // POST request
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(data => {
+      getPosts()
+    })
+    .catch(err => console.log(err))
+
+  e.preventDefault()
 }
